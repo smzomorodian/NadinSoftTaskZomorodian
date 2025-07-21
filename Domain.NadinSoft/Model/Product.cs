bba2c4e6a74d952df4bd1baf039c222128105413
mohammad zomorodian <smzomorodin@gmail.com>
2025-07-21 21:08:57 +0330

    ADDModelProduct

---
+++ Domain.NadinSoft/Model/Product.cs
@@ -0,0 +1,35 @@
+﻿using Domain.NadinSoft.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.NadinSoft.Model
+{
+    public class Product
+    {
+        public Product() { }
+        public Product(string name, DateTime produceDate, string manufacturePhone, string manufactureEmail, bool isAvailable/*, Guid userId*/)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            ProduceDate = produceDate;
+            ManufacturePhone = manufacturePhone;
+            ManufactureEmail = manufactureEmail;
+            IsAvailable = isAvailable;
+            //CreatedByUserId = userId;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public DateTime ProduceDate { get; private set; }
+        public string ManufacturePhone { get; private set;}
+        public string ManufactureEmail { get; private set; }
+        public bool IsAvailable { get; private set; }
+
+        //public Guid CreatedByUserId { get; private set; }
+        //public User user { get; private set; }
+    }
+}
+
--- Infrustructure.NadinSoft/Context/APPDbcontext.cs
+++ Infrustructure.NadinSoft/Context/APPDbcontext.cs
@@ -18,6 +18,29 @@ namespace Infrustructure.NadinSoft.Context
         {
         }
 
-        public DbSet<User> users { get; set; }
+        public DbSet<Product> products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+
+                entity.Property(p => p.ManufacturePhone)
+                .IsRequired().HasMaxLength(20);
+
+                entity.Property(p => p.ManufactureEmail)
+                .IsRequired().HasMaxLength(100);
+
+                entity.Property(p => p.Name)
+                .IsRequired().HasMaxLength(200);
+
+                entity.HasIndex(p => new { p.ManufactureEmail, p.ProduceDate })
+                .IsUnique();
+
+
+            });
+        }
     }
 }
