ff65ebaf68afb63d03a5be38dd0c62d66e5c9abe
mohammad zomorodian <smzomorodin@gmail.com>
2025-07-21 14:42:21 +0330

    Add project files.

---
+++ Domain.NadinSoft/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.NadinSoft.Model
+{
+    public class User
+    {
+        public User() { }
+
+        public User(string name, string nationalCode, string phoneNumber, DateTime? birthDay, string role, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            NationalCode = nationalCode;
+            PhoneNumber = phoneNumber;
+            BirthDay = birthDay;
+            Role = role;
+            Email = email;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string NationalCode { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public DateTime? BirthDay { get; private set; }
+        public string Role { get; private set; }
+        public string Email { get; private set; }
+        public string? Otp { get; private set; } // کد موقت برای بازنشانی رمز عبور
+        public DateTime? OtpExpiry { get; private set; } // تاریخ انقضا کد یک بار مصرف
+    }
+}
---
+++ Infrustructure.NadinSoft/Context/APPDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.NadinSoft.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.NadinSoft.Context
+{
+    public class APPDbcontext : DbContext
+    {
+        protected APPDbcontext()
+        {
+        }
+        public APPDbcontext(DbContextOptions<APPDbcontext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
